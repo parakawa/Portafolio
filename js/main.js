f74839a6c3d0f1e5b54e97c6e93f10e27dbc8c32
Paty Arakawa <paty.arakawa@gmail.com>
2016-06-17 11:02:08 -0500

    v. invidentes sin tunear

--- js/main.js
+++ js/main.js
@@ -73,6 +73,27 @@ function leerDiv(div){
     case "cip-text":
         url="https://translate.google.com/translate_tts?ie=UTF-8&q=Realic%C3%A9%20este%20proyecto%20en%20calidad%20de%20freelancer%20Trabajo%20de%20la%20mano%20con%20un%20backend%20para%20automatizar%20el%20despliegue%20de%20m%C3%A1s%20de%20300%20p%C3%A1ginas%20asociadas.&tl=es&total=2&idx=1&textlen=144&tk=650505.1039339&client=t&prev=input";
         break;
+    case "whatsapp":
+        url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=Whatsapp%20Web&tl=es&total=1&idx=0&textlen=12&tk=837437.717274&client=t&prev=input";
+        break;
+    case "whatsapp-text":
+        url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=%C3%9Altimo%20examen%20de%20Laboratoria%20en%20la%20cual%20apliqu%C3%A9%20todos%20los%20conocimientos%20adquiridos%20en%20manipulaci%C3%B3n%20del%20DOM%20con%20Javascript.%20En%20este%20examen%20me%20sirvi%C3%B3%20mucho%20la%20experiencia%20de%20ense%C3%B1ar%20en%20MakerLab.&tl=es&total=1&idx=0&textlen=192&tk=904148.785715&client=t&prev=input";
+        break;
+    case "contacto-text":
+        url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=contacto&tl=es&total=1&idx=0&textlen=8&tk=741155.883140&client=t&prev=input";
+        break;
+    case "numeros":
+        url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=numero%20de%20tel%C3%A9fono%20990548941&tl=es&total=1&idx=0&textlen=28&tk=674894.817833&client=t&prev=input";
+        break;
+    case "correo":
+        url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=correo%20paty.arakawa%40gmail.com&tl=es&total=1&idx=0&textlen=29&tk=818191.674536&client=t&prev=input";
+        break;
+    case "descarga":
+        url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=Aqu%C3%AD%20puedes%20descargar%20mi%20cv&tl=es&total=1&idx=0&textlen=27&tk=620240.999479&client=t&prev=input";
+        break;
+    case "descarga":
+        url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=Aqu%C3%AD%20puedes%20descargar%20mi%20cv&tl=es&total=1&idx=0&textlen=27&tk=620240.999479&client=t&prev=input";
+        break;
     default:
         alert("bug")
   }
@@ -91,7 +112,11 @@ function stop(){
 }
 
 function invidentes(){
-  alert("Estás en la versión para invidentes, pasa el mouse por algún texto para escuchar");
+    var audio = $("#audio")[0];
+    var url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=Bienvenido%20a%20mi%20portafolio!%20Est%C3%A1s%20en%20la%20versi%C3%B3n%20web%20para%20invidentes%2C%20al%20pasar%20el%20mouse%20por%20los%20textos%20escuchar%C3%A1s%20el%20contenido%20de%20los%20mismos.&tl=es&total=1&idx=0&textlen=140&tk=100891.507132&client=t&prev=input"
+      $("#audio").attr("src", url);
+
+      audio.play();
   $("#paty").attr("onmouseover", "leerDiv(this)");
   $("#paty").attr("onmouseout", "stop()");
   $("#desarrolladora").attr("onmouseover", "leerDiv(this)");
@@ -136,8 +161,6 @@ function invidentes(){
   $("#correo").attr("onmouseout", "stop()");
   $("#descarga").attr("onmouseover", "leerDiv(this)");
   $("#descarga").attr("onmouseout", "stop()");
-  $("#creditos").attr("onmouseover", "leerDiv(this)");
-  $("#creditos").attr("onmouseout", "stop()");
 }
 
 
