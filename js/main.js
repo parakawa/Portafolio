bde3e88d176547f76ffa4dba68a77ca5de76cccc
Paty Arakawa <paty.arakawa@gmail.com>
2016-06-16 19:15:17 -0500

    implementando función leerDiv

--- js/main.js
+++ js/main.js
@@ -13,3 +13,20 @@ $(function() {
     }
   });
 });
+
+
+function leerDiv(div){
+  var contenido= $(div).text();
+  var url="https://translate.google.com.pe/translate_tts?ie=UTF-8&q=Paty%20Arakawa&tl=es&total=1&idx=0&textlen=12&tk=67250.471141&client=t&prev=input";
+  var audio = $("#audio")[0];
+
+      $("#audio").attr("src", url);
+
+      audio.play();
+
+}
+
+function stop(){
+  var audio = $("#audio")[0];
+    audio.pause();
+}
